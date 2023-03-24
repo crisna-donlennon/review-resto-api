2711320bf8f9f592a289470825849e2edc17293b
crisna-donlennon <krisnafajar148@gmail.com>
2023-03-24 09:19:11 +0700

    coom

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -5,16 +5,65 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use app\Models\User;
+use Laravel\Sanctum\Sanctum;
 
 class AuthenticationTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_user_can_login()
     {
-        $response = $this->get('/');
+        $user = User::factory()->createOne();
 
-        $response->assertStatus(200);
+        $data = [
+            'email' => $user->email,
+            'password' => 'password',
+            'device_name' => 'testing',
+        ];
+
+        $this->postJson(route('auth.login'), $data)
+            ->assertOk()
+            ->assertJsonFragment(['access_token', 'user']);
+    }
+
+    public function test_user_can_register()
+    {
+        $data = [
+            'name' => 'Tester',
+            'email' => 'tester@email',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        $this->postJson(route('auth.register'), $data)
+            ->assertCreated()
+            ->assertJsonFragment(['email' => $data['email']]);
+    }
+
+    public function test_user_can_see_their_profile()
+    {
+        $user = User::factory()->createOne();
+
+        Sanctum::actingAs($user);
+
+        $this->getJson(route('auth.profile'))
+            ->assertOk()
+            ->assertJsonFragment(['email' => $user->email]);
+    }
+
+    public function test_user_cannot_see_their_profile_when_unauthenticated()
+    {
+        $this->getJson(route('auth.profile'))->assertUnauthorized();
+    }
+    
+    public function test_user_can_logout()
+    {
+        $user = User::factory()->createOne();
+
+        Sanctum::actingAs($user);
+
+        $this->getJson(route('auth.logout'))->assertOk();
     }
 }
